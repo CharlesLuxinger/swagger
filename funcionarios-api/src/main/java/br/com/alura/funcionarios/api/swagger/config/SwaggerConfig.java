b5e46fda194efe01a5bdac02cfeca3ba19dff27c
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-10 20:14:48 -0300

    16. Swagger Info Config

--- funcionarios-api/src/main/java/br/com/alura/funcionarios/api/swagger/config/SwaggerConfig.java
+++ funcionarios-api/src/main/java/br/com/alura/funcionarios/api/swagger/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package br.com.alura.funcionarios.api.swagger.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +20,15 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("br.com.alura.funcionarios.api"))
+				.build().apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("Funcionários API").description("Api de controle de funcionários")
+				.contact(new Contact("Charles Luxinger", 
+									 "https://www.linkedin.com/in/charlesluxinger/",
+									 "charlesluxinger@gmail.com"))
+				.version("1.0.0")
 				.build();
 	}
 }
